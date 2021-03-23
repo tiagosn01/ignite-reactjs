dfd44660dfc49ca0b3f859bc85e82083ed54307e
tiagosn01 <tiagosilva0922@gmail.com>
2021-03-22 23:55:15 -0300

    feat: starting classes on the Ignews project

---
+++ ignews/src/pages/index.tsx
@@ -0,0 +1,15 @@
+
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Inicio | Ignews</title>
+      </Head>
+        <h1>
+          Hellow World
+        </h1>
+    </>
+  )
+}
