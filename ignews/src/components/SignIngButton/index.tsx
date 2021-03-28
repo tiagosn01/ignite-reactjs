1b51d3b9cefad5cf73404016395e9d813ca75f03
tiagosn01 <tiagosilva0922@gmail.com>
2021-03-28 17:18:09 -0300

    feat: cominucattion with faunadb

--- ignews/src/components/SignIngButton/index.tsx
+++ ignews/src/components/SignIngButton/index.tsx
@@ -1,19 +1,27 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, useSession, signOut } from 'next-auth/client'
+
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUser = true;
-
+  const [session] = useSession();
+  console.log(session);
   return (
-   isUser ? (
-    <button className={styles.signInButton}>
+    session ? (
+    <button 
+    className={styles.signInButton}
+    onClick={() => signOut()}
+    >
     <FaGithub color="#04d361" />
-     Tiago Silva
+     {session.user.name}
     <FiX color="#737380" className={styles.closeIcon}/>
   </button>
    ) : (
-    <button className={styles.signInButton}>
+    <button 
+    className={styles.signInButton}
+    onClick={() => signIn('github')}
+    >
     <FaGithub color="#eba417" />
     Sign in with Guthub
   </button>
